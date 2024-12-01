f8088bd9accad09932843c148fad3947b517604b
Yurka2012 <tsapiy.yurka@gmail.com>
2024-12-01 15:53:14 +0200

    cardRenderStart

--- src/js/card.js
+++ src/js/card.js
@@ -1,8 +1,18 @@
 import debounce from 'debounce';
 import axios from "axios";
+import handlebars from "handlebars";
+
+document.addEventListener("DOMContentLoaded", searchDataOnLoad)
+
+async function searchDataOnLoad() {
+    let dataFromAxios = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=GuFBpVgQ1pTfyV0ZT9FA4QxjKXxhnZzD&keyword=shit`)
+    console.log(dataFromAxios)
+}
+
+
 
 let searchInput = document.querySelector(".searchInput")
-let searchInputValue = "Eurovision"
+let searchInputValue = "Shit"
 
 function collectInpupData(){
     searchInputValue = document.querySelector(".searchInput").value
@@ -11,9 +21,16 @@ function collectInpupData(){
     searchData()
 }
 
-searchInput.addEventListener("input", debounce(collectInpupData, 100))
+searchInput.addEventListener("input", debounce(collectInpupData, 500))
 
 async function searchData() {
-    let dataFromAxios = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=GuFBpVgQ1pTfyV0ZT9FA4QxjKXxhnZzD/keyword=${searchInputValue}`)
-    console.log(dataFromAxios)
+    let dataFromAxios = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=GuFBpVgQ1pTfyV0ZT9FA4QxjKXxhnZzD&keyword=${searchInputValue}`)
+    let shit = document.createElement("img")
+    shit.src = dataFromAxios.data._embedded.events[0].images[0].url
+    console.log(shit)
+    let main = document.querySelector(".main")
+    main.innerHTML = shit
+
+    console.log(dataFromAxios.data._embedded.events)
 }
+
